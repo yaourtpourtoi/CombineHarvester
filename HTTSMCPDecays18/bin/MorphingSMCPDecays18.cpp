10d86a73177f127d843304358e17134f8c18c930
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2020-03-13 18:20:34 +0000

    few updates

--- HTTSMCPDecays18/bin/MorphingSMCPDecays18.cpp
+++ HTTSMCPDecays18/bin/MorphingSMCPDecays18.cpp
@@ -368,7 +368,8 @@ int main(int argc, char** argv) {
     input_dir["ttbar"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/HTTSMCPDecays18/shapes/"+input_folder_em+"/";    
     
     
-    VString chns = {"tt","mt"};
+    //VString chns = {"tt","mt"};
+    VString chns = {"tt"};
     if (ttbar_fit) chns.push_back("ttbar");
     
     map<string, VString> bkg_procs;
